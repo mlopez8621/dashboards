1a7527d2abd6270c4e72a63650f691e321692a3d
michel <michelini19a@gmail.com>
2019-11-19 16:31:30 -0500

    listo3

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('test', { title: 'Express - Michel' });
+  res.render('index', { title: 'Express - Michel' });
 });
 
 module.exports = router;
