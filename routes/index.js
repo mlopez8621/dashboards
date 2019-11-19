82f954ccc8f61767825d38edcec62a1d2ac58d3e
michel <michelini19a@gmail.com>
2019-11-19 16:01:44 -0500

    test2

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
 
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('test', { title: 'Express - Michel' });
 });
 
-module.exports = router;
+module.exports = router;*/
+
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('michel-leonardo-lopez-avendao.rstudio.cloud.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
